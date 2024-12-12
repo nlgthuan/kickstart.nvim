fe6ea70785753578851cc555bb0b653cb41c194a
thuan.ngo <thuan.ngo@eastagile.com>
2024-12-12 23:12:51 +0700

    [f] - Add more keymap

--- init.lua
+++ init.lua
@@ -153,9 +153,17 @@ vim.opt.scrolloff = 10
 --  See `:help vim.keymap.set()`
 --
 
+-- Greatest keymap evaaaa
 vim.keymap.set('n', '<leader>gl', vim.diagnostic.open_float)
-
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
