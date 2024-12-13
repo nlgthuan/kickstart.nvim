466873d07813acb6b7004fd737e25cfeab50f373
thuan.ngo <thuan.ngo@eastagile.com>
2024-12-13 09:43:27 +0700

    [f] - Add diagnostic keys

--- init.lua
+++ init.lua
@@ -156,6 +156,8 @@ vim.opt.scrolloff = 10
 -- Greatest keymap evaaaa
 vim.keymap.set('n', '<leader>gl', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
